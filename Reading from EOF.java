3ef027dde948f47ce55dbc4155427f42e3602f9d
Perksha <158833062+Perksha@users.noreply.github.com>
2024-05-09 17:13:31 +0530

    Create Reading from EOF.java

---
+++ Reading from EOF.java
@@ -0,0 +1,21 @@
+The challenge here is to read  lines of input until you reach EOF, then number and print all  lines of content.
+
+Hint: Java's Scanner.hasNext() method is helpful for this problem.
+
+Input Format
+
+Read some unknown  lines of input from stdin(System.in) until you reach EOF; each line of input contains a non-empty String.
+
+public class Solution {
+
+    public static void main(String[] args) {
+        /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution. */
+        Scanner s= new Scanner(System.in);
+        int i=1;
+        while(s.hasNext()){
+          String line= s.nextLine();
+          System.out.println(i+" "+ line);
+          i++;  
+        }
+    }
+}
