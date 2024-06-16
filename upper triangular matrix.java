b0207e3c94839efa10c1ce251f49ba4dbcfbfc00
Perksha <158833062+Perksha@users.noreply.github.com>
2024-06-16 19:12:03 +0530

    Create upper triangular matrix.java

---
+++ upper triangular matrix.java
@@ -0,0 +1,31 @@
+import java.util.*;
+class HelloWorld {
+    public static void main(String[] args) {
+        Scanner s=new Scanner(System.in);
+        int rac=s.nextInt();
+        int[][] arr=new int[rac][rac];
+        for(int i=0;i<rac;i++){
+            for(int j=0;j<rac;j++){
+                arr[i][j]=s.nextInt();
+            }
+        }
+        System.out.println("ORIGINAL MATRIX");
+        for(int i=0;i<rac;i++){
+            for(int j=0;j<rac;j++){
+                System.out.print(arr[i][j]+" ");
+            }
+            System.out.println();
+        }
+         System.out.println("UPPER TRIANGULAR MATRIX");
+        for(int i=0;i<rac;i++){
+            for(int j=0;j<rac;j++){
+                if(i<=j){
+                    System.out.print(arr[i][j]+" ");
+                }else{
+                    System.out.print(0+" ");
+                }
+            }
+            System.out.println();
+        }
+    }
+}
