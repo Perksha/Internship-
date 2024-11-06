ecbe24e22a4b911f1337f4caa72be08252ed00ab
Perksha <158833062+Perksha@users.noreply.github.com>
2024-11-06 15:39:58 +0530

    Update merge sort.java

--- merge sort.java
+++ merge sort.java
@@ -1,54 +1,37 @@
-import java.util.Scanner;
-class HelloWorld {
-    public static void mergesort(int[] arr,int i,int j){
-        if(i==j) return;
-        int mid=(i+j)/2;
-        mergesort(arr,i,mid);
-        mergesort(arr,mid+1,j);
-        merge(arr,i,mid,j);
-        
+class Solution {
+    public  static void mergesort(int[]  nums,int l,int r){
+        if(l>=r) return;
+            int mid=(l+r)/2;
+            mergesort(nums,l,mid);
+            mergesort(nums,mid+1,r);
+            merge(nums,l,mid,r);
     }
-    public static void merge(int arr[],int i,int mid,int j){
-        int p1=i,p2=mid+1,e1=mid,e2=j;
-        int y=i;
-        int[] b=new int[arr.length]; 
-        while(p1<=e1&&p2<=e2){
-            if(arr[p1]<arr[p2]){
-                b[y]=arr[p1];
-                y++;
-                p1++;
+    public static void merge(int[] nums,int l,int mid,int r){
+        int s1=l;
+        int e1=mid;
+        int s2=mid+1;
+        int e2=r;
+        int idx=0;
+        int arr[]=new int[r-l+1];
+        while(s1<=e1&&s2<=e2){
+            if(nums[s1]<nums[s2]){
+                arr[idx++]=nums[s1++];
             }else{
-                b[y]=arr[p2];
-                p2++;
-                y++;
+                arr[idx++]=nums[s2++];
             }
         }
-        while(p1<=e1){
-            b[y]=arr[p1];
-            y++;
-            p1++;
+        while(s1<=e1){
+            arr[idx++]=nums[s1++];
         }
-        while(p2<=e2){
-            b[y]=arr[p2];
-            y++;
-            p2++;
+        while(s2<=e2){
+            arr[idx++]=nums[s2++];
         }
-        for(int r=i;r<=j;r++){
-            arr[r]=b[r];
+        for(int i=0;i<arr.length;i++){
+            nums[l+i]=arr[i];
         }
     }
-    public static void main(String[] args) {
-        Scanner s=new Scanner(System.in);
-        int n=s.nextInt();
-        int[] arr=new int[n];
-        for(int i=0;i<n;i++){
-            arr[i]=s.nextInt();
-        }
-        System.out.println("Sorted array");
-        mergesort(arr,0,n-1);
-        for(int j=0;j<n;j++){
-          System.out.print(arr[j]+" ");
-        }
+    public void sortColors(int[] nums) {
+      mergesort(nums,0,nums.length-1);  
     }
 }
 
